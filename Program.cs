d54b3d375c265ff1aabe25a01d70f41a1957cdd1
Melissa Martins <melmartins.pro@gmail.com>
2026-02-17 11:24:15 -0300

    pasta de metodos adicionados, e uma lista com historicos de jogos anteriores

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 ﻿
 List<int> jogosAnteriores = new();
-
 Console.WriteLine("Bem vindo ao Jogo Da Matemática!");
 Console.WriteLine("Vamos começar?");
 Console.WriteLine("\t1 - Sim");
 Console.WriteLine("\t2 - Não");
 var permissãoPraJogar = Console.ReadLine();
 
-if(permissãoPraJogar == "1")
+if (permissãoPraJogar == "1")
 {
-    Console.WriteLine("Vamos lá, mas antes uma breve instrução:" );
+    Console.WriteLine("Vamos lá, mas antes uma breve instrução:");
     Console.WriteLine("O jogo tem 5 questões aritméticas de sua escolha, a cada resposta correta voce ganhrá um ponto!");
     Console.WriteLine("----------------------------------------");
     Console.WriteLine("Escolha com que tipo de operação aritmética quer jogar:");
@@ -19,46 +18,61 @@ if(permissãoPraJogar == "1")
     Console.WriteLine("\t4 - Divisão");
     var tipoDeOperaçãoEscolhida = Console.ReadLine();
 
-    switch(tipoDeOperaçãoEscolhida)
+    switch (tipoDeOperaçãoEscolhida)
     {
         case "1":
-            Console.WriteLine("O jogo da soma vai começar!");
-            var contagemDePontos = 0;
 
-            for (int i = 0; i <= 5; i++)
             {
-                
-                Random valor = new Random();
-                int valores = valor.Next(0, 100);
-                int valores2 = valor.Next(0,100);
-                Console.WriteLine($"Qual é o resultado da soma entre {valores} + {valores2}");
-                var resposta = Convert.ToInt32(Console.ReadLine());
-                var resultadointerno = valores + valores2;
+                Console.WriteLine("O jogo da soma vai começar!");
+                var contagemDePontos = 0;
 
-                
-                jogosAnteriores.Add(resultadointerno);
-                if(resposta == resultadointerno)
+
+                for (int i = 1; i <= 5; i++)
                 {
-                    Console.WriteLine("Certa resposta! +1 ponto ");
-                    contagemDePontos++;
+                    Random valor = new Random();
+                    int valores = valor.Next(0, 100);
+                    int valores2 = valor.Next(0, 100);
+
+                    Console.WriteLine($"Qual é o resultado da soma entre {valores} + {valores2}");
+                    var resposta = Convert.ToInt32(Console.ReadLine());
+                    int resultadointerno = valores + valores2;
+
+
+                    jogosAnteriores.Add(resultadointerno);
+
+                    if (resposta == resultadointerno)
+                    {
+                        Console.WriteLine("Certa resposta! +1 ponto ");
+                        contagemDePontos++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resposta incorreta -1 ponto");
+                        contagemDePontos--;
+                    }
+
+
+
+                }
+                Console.WriteLine($"Total de pontos:{contagemDePontos}");
+                Console.WriteLine("Deseja ver o resultado de jogos anteriores?");
+                Console.WriteLine("\tS -Sim");
+                Console.WriteLine("\tN -Não");
+                var historico = Console.ReadLine();
+
+                if (historico == ("S").ToLower())
+                {
+                    Console.WriteLine($"Resultado de jogos Anteriores : {jogosAnteriores}");
                 }
                 else
                 {
-                    Console.WriteLine("Resposta incorreta -1 ponto");
-                    contagemDePontos--;
+                    Environment.Exit(0);
                 }
-               
+
             }
-            Console.WriteLine($"Total de pontos:{contagemDePontos}");
-            
-            foreach(var resultado in jogosAnteriores)
-            {
-                Console.WriteLine($"Resultdos dos jogos anteriores: {resultado}");
-            }
-           
-            ;
+                ;
             break;
-            case "2":
+        case "2":
 
             Console.WriteLine("O jogo da subtração vai começar!");
             var contagemDePontos1 = 0;
@@ -87,14 +101,16 @@ if(permissãoPraJogar == "1")
             }
 
 
-            
+
 
             Console.WriteLine($"Total de pontos:{contagemDePontos1}");
 
             //se o primeiro valor aleatorio for menor que o segundo ,eu troco eles de lugar
+            //SE O RESULTADO FOR UM DECIMAL,O LOOP DEVE CONTINUAR ATÉ ME DEVOLVE UM RESULTADO INTEIRO
             ;
             break;
-            case "3":;
+        case "3":
+            ;
 
             Console.WriteLine("O jogo da Multiplicação vai começar!");
             var contagemDePontos2 = 0;
@@ -124,7 +140,7 @@ if(permissãoPraJogar == "1")
             Console.WriteLine($"Total de pontos:{contagemDePontos2}");
 
             break;
-            case "4":
+        case "4":
 
             Console.WriteLine("O jogo da Divisão vai começar!");
             var contagemDePontos3 = 0;
@@ -137,9 +153,9 @@ if(permissãoPraJogar == "1")
                 int valores2 = valor.Next();// o resultado deve sempre ser inteiro
                 Console.WriteLine($"Qual é o resultado da divisão entre {valores} / {valores2}");
                 var resposta = Convert.ToInt32(Console.ReadLine());
-                var resultadointerno = valores / valores2 ;
+                var resultadointerno = valores / valores2;
 
-              //se o resto da divisão nao for igual a 0, o jogo deve recomeçar
+                //se o resto da divisão nao for igual a 0, o jogo deve recomeçar
 
                 if (resposta == resultadointerno)
                 {
